7565806b0407019137a53711109470e05e326560
Christian Olsen <9137189+Chrede88@users.noreply.github.com>
2023-10-04 19:41:34 +0200

    adding warning if no VPN's are found

--- vpnlist.sh
+++ vpnlist.sh
@@ -15,16 +15,21 @@ vpn_list() {
 
   # output JSON
   jsonstr="{\"items\":["
-  for vpn in "${vpns[@]}";do
-    conn=$(get_vpn_status $vpn )
-    jsonstr+="{\"title\":\"$vpn\",\"subtitle\":\"$conn\",\"arg\":\"$vpn\",";
-    if [[ "$conn" == "Connected" ]]; then
-      jsonstr+="\"icon\":{\"path\":\"./images/on.png\"}"
-    else
-      jsonstr+="\"icon\":{\"path\":\"./images/off.png\"}"
-    fi
-    jsonstr+="},"
-  done
+  if [ ${#vpns[@]} == 0 ]; then
+    jsonstr+="{\"title\":\"No VPN's found!\",\"arg\":\"NoVPNs\"}"
+  else
+    for vpn in "${vpns[@]}";do
+      conn=$(get_vpn_status $vpn )
+      jsonstr+="{\"title\":\"$vpn\",\"subtitle\":\"$conn\",\"arg\":\"$vpn\",";
+      if [[ "$conn" == "Connected" ]]; then
+        jsonstr+="\"icon\":{\"path\":\"./images/on.png\"}"
+      else
+        jsonstr+="\"icon\":{\"path\":\"./images/off.png\"}"
+      fi
+      jsonstr+="},"
+    done
+  fi
+
   jsonstr="${jsonstr%,}"
   jsonstr+="]}"
   echo -e $jsonstr
