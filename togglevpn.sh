4c2632c43cc0a0018e4525fc20a7998fed492cef
Christian Olsen <christian@cjsolsen.com>
2025-12-23 11:13:31 +0100

    feat: close open connections before starting a new
    Signed-off-by: Christian Olsen <christian@cjsolsen.com>

--- togglevpn.sh
+++ togglevpn.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+get_vpn_names() {
+  scutil --nc list | grep "com.wireguard.macos" | awk -F'"' '{print$2}' | sort
+}
+
 get_vpn_status() {
   local vpn=$1
   scutil --nc status "$vpn" | head -n 1 | grep -i "connected"
@@ -15,17 +19,55 @@ disconnect_vpn() {
   scutil --nc stop "$vpn"
 }
 
+wait_for_disconnect() {
+    local vpn=$1
+    status=$(get_vpn_status "$vpn")
+
+    timeout=0
+    while [ "$status" = "" ]
+    do
+        sleep 0.5
+        timeout=$(echo $timeout + 0.5 | bc)
+        if [ timeout > 5 ]; then
+            break
+        fi
+        status=$(get_vpn_status "$vpn")
+    done
+
+    timeout=0
+    while [ "$status" = "Connected" ]
+    do
+        sleep 0.5
+        timeout=$(echo $timeout + 0.5 | bc)
+        if [ timeout > 5 ]; then
+            break
+        fi
+        status=$(get_vpn_status "$vpn")
+    done
+}
+
 toggle_vpn() {
-  local vpn="$1"
-  conn=$(get_vpn_status "$vpn")
+  local new_vpn="$1"
+  conn=$(get_vpn_status "$new_vpn")
+
+  IFS=$'\n' read -r -d '' -a vpns < <( get_vpn_names && printf '\0' )
 
   # toggle state
-  if [[ "$conn" == "Connected" ]]; then
-    disconnect_vpn "$vpn"
-    echo "$vpn is disconnected"
+  if [ "$conn" = "Connected" ]; then
+    disconnect_vpn "$new_vpn"
+    echo "$new_vpn is disconnected"
   else
-    connect_vpn "$vpn"
-    echo "$vpn is connected"
+    # check if other connection are open and close them first
+    for vpn in "${vpns[@]}";do
+      conn_old=$(get_vpn_status "$vpn")
+      if [ "$conn_old" = "Connected" ]; then
+        disconnect_vpn "$vpn"
+        wait_for_disconnect "$vpn"
+      fi
+    done
+
+    connect_vpn "$new_vpn"
+    echo "$new_vpn is connected"
   fi
 }
 
